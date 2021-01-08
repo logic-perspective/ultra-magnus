f358a84968fb127db4b5dd27b16c46f3c0ef09ab
lloydtawanda <lloydmatereke23@gmail.com>
2021-01-09 01:14:15 +0200

    added web routes for vancouver and chicago controllers
    Signed-off-by: lloydtawanda <lloydmatereke23@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,14 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::prefix('chicago-crime')->group(function () {
+    Route::get('violent', 'ChicagoController@getViolentCrimeCount');
+    Route::get('property', 'ChicagoController@getPropertyCrimeCount');
+    Route::get('consensual', 'ChicagoController@getConsensualCrimeCount');
+    Route::get('organised', 'ChicagoController@getOrganisedCrimeCount');
+    Route::get('district-rates', 'ChicagoController@getDistrictRates');
+});
+
+Route::resource('chicago-crime', ChicagoController::class);
+
+Route::resource('vancouver-crime', VancouverController::class);
