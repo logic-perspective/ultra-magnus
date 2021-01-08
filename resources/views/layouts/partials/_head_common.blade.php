8fd24facb61043c9ea9be115dae09661a04878b8
lloydtawanda <lloydmatereke23@gmail.com>
2021-01-09 01:10:48 +0200

    updated logo
    Signed-off-by: lloydtawanda <lloydmatereke23@gmail.com>

--- resources/views/layouts/partials/_head_common.blade.php
+++ resources/views/layouts/partials/_head_common.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<title>@hasSection('metaTitle') @yield('metaTitle') @else {{ config('app.name', 'Point Crime') }} @endif</title>
+<title>Point Crime</title>
 
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no">
