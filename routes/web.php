faa5893c91950eb974ff5a9b31e704d4612c6346
lloydtawanda <lloydmatereke23@gmail.com>
2021-01-09 18:42:25 +0200

    updated db connection variables
    Signed-off-by: lloydtawanda <lloydmatereke23@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::prefix('/chicago-crime')->group(function () {
-    Route::get('/violent', 'ChicagoController@getViolentCrimeCount')->name('chicago-crime.violent');
+Route::prefix('chicago-crime')->group(function () {
+    Route::get('violent', 'ChicagoController@getViolentCrimeCount')->name('chicago-crime.violent');
     Route::get('property', 'ChicagoController@getPropertyCrimeCount')->name('chicago-crime.property');
     Route::get('consensual', 'ChicagoController@getConsensualCrimeCount')->name('chicago-crime.consensual');;
     Route::get('organised', 'ChicagoController@getOrganisedCrimeCount')->name('chicago-crime.organised');;
